36f596498c0c54dd8420746f22ec44cacc0994a9
HyRALis <petar.trajanoski.pt@gmail.com>
2020-10-24 23:36:05 +0200

    Navbar done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,20 +7,22 @@ import styles from "../styles/modules/Navbar.module.scss";
 
 export const Navbar = () => {
   return (
-    <div className={styles.navbar}>
-      <ul className={styles.main}>
-        <NavItem svg="Home" />
-        <NavItem svg="About" />
-        <NavItem svg="Projets" />
-        <NavItem svg="Mail" />
-      </ul>
-      <ul className={styles.social}>
-        <NavItem svg="LinkedIn" />
-        <NavItem svg="Github" />
-        <NavItem svg="Facebook" />
-        <NavItem svg="Instagram" />
-        <NavItem svg="Twitter" />
-      </ul>
+    <div className={styles.navbar_ilumination}>
+      <div className={styles.navbar}>
+        <ul className={styles.main}>
+          <NavItem svg="Home" />
+          <NavItem svg="About" />
+          <NavItem svg="Projets" />
+          <NavItem svg="Mail" />
+        </ul>
+        <ul className={styles.social}>
+          <NavItem svg="LinkedIn" />
+          <NavItem svg="Github" />
+          <NavItem svg="Facebook" />
+          <NavItem svg="Instagram" />
+          <NavItem svg="Twitter" />
+        </ul>
+      </div>
     </div>
   );
 };
