7c9a996e9aa9ca35bc7c59bed249b318c5e573fb
HyRALis <petar.trajanoski.pt@gmail.com>
2020-11-01 13:21:19 +0100

    Made Navbar a global Component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import { useState, useEffect, Fragment } from "react";
+
+import Navbar from "../components/Navbar";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [windowWidth, setWindowWidth] = useState(null);
+
+  function determineWidth() {
+    const width = window.innerWidth;
+    setWindowWidth(width);
+  }
+  useEffect(() => {
+    determineWidth();
+    window.addEventListener("resize", determineWidth);
+
+    return function () {
+      window.removeEventListener("resize", determineWidth);
+    };
+  }, []);
+
+  return (
+    <Fragment>
+      <Navbar width={windowWidth} />
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,27 @@ import { useState, useEffect } from "react";
 import Head from "next/head";
 
 import Navbar from "../components/Navbar";
-import MainPattern from "../public/assets/patterns/mainPatern.svg";
+import PatternHome from "../components/svg/PatternHome";
+// import MainPattern from "../public/assets/patterns/mainPatern.svg";
 
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
+import { motion } from "framer-motion";
 
 export default function Home() {
-  const [windowWidth, setWindowWidth] = useState(null);
+  // const [windowWidth, setWindowWidth] = useState(null);
 
-  function determineWidth() {
-    const width = window.innerWidth;
-    setWindowWidth(width);
-  }
-  useEffect(() => {
-    determineWidth();
-    window.addEventListener("resize", determineWidth);
+  // function determineWidth() {
+  //   const width = window.innerWidth;
+  //   setWindowWidth(width);
+  // }
+  // useEffect(() => {
+  //   determineWidth();
+  //   window.addEventListener("resize", determineWidth);
 
-    return function () {
-      window.removeEventListener("resize", determineWidth);
-    };
-  }, []);
+  //   return function () {
+  //     window.removeEventListener("resize", determineWidth);
+  //   };
+  // }, []);
 
   return (
     <div className={styles.container}>
@@ -28,7 +30,15 @@ export default function Home() {
         <title>Petar T. Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar width={windowWidth} />
+      {/* <Navbar width={windowWidth} /> */}
+      <main className={styles.main}>
+        <PatternHome height={"45%"} />
+        <div className={styles.content}>
+          <motion.h1>Petar</motion.h1>
+          <motion.h2>Trajanoski</motion.h2>
+          <motion.h3>Front-end developer & UI designer</motion.h3>
+        </div>
+      </main>
     </div>
   );
 }
