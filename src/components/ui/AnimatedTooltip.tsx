943d7d8d8c947d59da6d9c4861122fe34eeeed17
Petar Trajanoski <petar.trajanoski.pt@gmail.com>
2025-01-14 17:18:57 +0100

    VERSION-2: Added animated tooltip to skills

--- src/components/ui/AnimatedTooltip.tsx
+++ src/components/ui/AnimatedTooltip.tsx
@@ -1,19 +1,14 @@
 'use client';
-import Image from 'next/image';
-import React, { useState } from 'react';
+
+import React, { PropsWithChildren, useState } from 'react';
 import { motion, useTransform, AnimatePresence, useMotionValue, useSpring } from 'framer-motion';
 
-export const AnimatedTooltip = ({
-    items
-}: {
-    items: {
-        id: number;
-        name: string;
-        designation: string;
-        image: string;
-    }[];
-}) => {
-    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+export interface AnimatedTooltipProps extends PropsWithChildren {
+    description: string;
+}
+
+export const AnimatedTooltip: React.FC<AnimatedTooltipProps> = ({ description, children }) => {
+    const [isHovered, setIsHovered] = useState<boolean>(false);
     const springConfig = { stiffness: 100, damping: 5 };
     const x = useMotionValue(0); // going to set this value on mouse move
     // rotate the tooltip
@@ -27,53 +22,41 @@ export const AnimatedTooltip = ({
     };
 
     return (
-        <>
-            {items.map((item) => (
-                <div
-                    className="-mr-4  relative group"
-                    key={item.name}
-                    onMouseEnter={() => setHoveredIndex(item.id)}
-                    onMouseLeave={() => setHoveredIndex(null)}
-                >
-                    <AnimatePresence mode="popLayout">
-                        {hoveredIndex === item.id && (
-                            <motion.div
-                                initial={{ opacity: 0, y: 20, scale: 0.6 }}
-                                animate={{
-                                    opacity: 1,
-                                    y: 0,
-                                    scale: 1,
-                                    transition: {
-                                        type: 'spring',
-                                        stiffness: 260,
-                                        damping: 10
-                                    }
-                                }}
-                                exit={{ opacity: 0, y: 20, scale: 0.6 }}
-                                style={{
-                                    translateX: translateX,
-                                    rotate: rotate,
-                                    whiteSpace: 'nowrap'
-                                }}
-                                className="absolute -top-16 -left-1/2 translate-x-1/2 flex text-xs  flex-col items-center justify-center rounded-md bg-black z-50 shadow-xl px-4 py-2"
-                            >
-                                <div className="absolute inset-x-10 z-30 w-[20%] -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px " />
-                                <div className="absolute left-10 w-[40%] z-30 -bottom-px bg-gradient-to-r from-transparent via-purple-500 to-transparent h-px " />
-                                <div className="font-bold text-white relative z-30 text-base">{item.name}</div>
-                                <div className="text-white text-xs">{item.designation}</div>
-                            </motion.div>
-                        )}
-                    </AnimatePresence>
-                    <Image
-                        onMouseMove={handleMouseMove}
-                        height={100}
-                        width={100}
-                        src={item.image}
-                        alt={item.name}
-                        className="object-cover !m-0 !p-0 object-top rounded-full h-14 w-14 border-2 group-hover:scale-105 group-hover:z-30 border-white  relative transition duration-500"
-                    />
-                </div>
-            ))}
-        </>
+        <div
+            className="relative group"
+            key={`${description}`}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            <AnimatePresence mode="popLayout">
+                {isHovered && (
+                    <motion.div
+                        initial={{ opacity: 0, y: 20, scale: 0.6 }}
+                        animate={{
+                            opacity: 1,
+                            y: 0,
+                            scale: 1,
+                            transition: {
+                                type: 'spring',
+                                stiffness: 260,
+                                damping: 10
+                            }
+                        }}
+                        exit={{ opacity: 0, y: 20, scale: 0.6 }}
+                        style={{
+                            translateX: translateX,
+                            rotate: rotate,
+                            whiteSpace: 'nowrap'
+                        }}
+                        className="absolute -top-2 -right-2 translate-x-1/2 flex text-xs  flex-col items-center justify-center rounded-md bg-black z-50 shadow-xl px-4 py-2"
+                    >
+                        <div className="absolute inset-x-10 z-30 w-[20%] -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px " />
+                        <div className="absolute left-10 w-[40%] z-30 -bottom-px bg-gradient-to-r from-transparent via-purple-500 to-transparent h-px " />
+                        <div className="font-bold text-white relative z-30 text-base">{description}</div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+            <div onMouseMove={handleMouseMove}>{children}</div>
+        </div>
     );
 };
--- src/components/ui/sections/Skills/Skills.tsx
+++ src/components/ui/sections/Skills/Skills.tsx
@@ -5,19 +5,22 @@ import { GradientText } from '../../GradientText';
 import { AnimatedCardsHover } from './AnimatedCardsHover';
 import { skills } from '@/utils/constants/skills';
 import { SkillsCard } from './SkillsCard';
+import { AnimatedTooltip } from '../../AnimatedTooltip';
 
 export const Skills = () => {
     const skillCards = skills.map(({ title, icon }) => ({
         title: title,
         content: (
-            <SkillsCard
-                key={title}
-                className={`text-white flex items-center justify-center bg-neutral-100 dark:bg-neutral-800${
-                    title === 'ChakraUi' ? ' text-neutral-800 dark:text-neutral-500' : ''
-                }`}
-            >
-                {icon}
-            </SkillsCard>
+            <AnimatedTooltip description={title}>
+                <SkillsCard
+                    key={title}
+                    className={`text-white flex items-center justify-center bg-neutral-100 dark:bg-neutral-800${
+                        title === 'ChakraUi' ? ' text-neutral-800 dark:text-neutral-500' : ''
+                    }`}
+                >
+                    {icon}
+                </SkillsCard>
+            </AnimatedTooltip>
         )
     }));
 
