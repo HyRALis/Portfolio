ba17ef0d9bfa92e4d86d02a0bba51237f423fb44
Petar Trajanoski <petar.trajanoski.pt@gmail.com>
2025-01-16 16:14:43 +0100

    VERSION-2: Fix for Mobile

--- src/components/ui/sections/WorkExperience/WorkExperience.tsx
+++ src/components/ui/sections/WorkExperience/WorkExperience.tsx
@@ -8,6 +8,16 @@ export const WorkExperience = () => {
     return (
         <Section className="space-y-10">
             <GradientText text="Work Experience" />
+            <div className="w-full bg-white dark:bg-neutral-950 flex md:hidden flex-col gap-4 justify-center items-center">
+                {workExperience.map((item) => (
+                    <div key={item.title} className="flex flex-col space-y-4">
+                        <h3 className="text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
+                            {item.title}
+                        </h3>
+                        {item.content}
+                    </div>
+                ))}
+            </div>
             <Timeline data={workExperience} />
         </Section>
     );
