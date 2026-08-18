1dae9c4cca4f408a31320aeabe711eb1fc9eaac5
HyRALis <petar.trajanoski.pt@gmail.com>
2026-08-18 23:39:47 +0200

    feature: Generative Engine Optimization

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,25 @@ import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/'
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/'
+            },
+            {
+                userAgent: [
+                    'GPTBot',
+                    'ChatGPT-User',
+                    'Google-Extended',
+                    'PerplexityBot',
+                    'ClaudeBot',
+                    'Applebot-Extended',
+                    'Amazonbot',
+                    'Meta-ExternalAgent'
+                ],
+                allow: '/'
+            }
+        ],
         sitemap: 'https://petartrajanoski.me/sitemap.xml'
     };
 }
